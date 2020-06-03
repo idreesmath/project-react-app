52d0a21d59471ac9f07778b96bd7fb99d7497e1c
Muhammad Idrees <idrees.math@hotmail.com>
2020-06-03 18:38:36 +0500

    Update app.js
    Updated app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br/>
+          <strong>Updated by Muhammad Idrees</strong>
         </p>
         <a
           className="App-link"
